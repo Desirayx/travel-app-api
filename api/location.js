429c8c99315a7cc5d3f1c6aca8c80808660d4275
Desirayx <forsythellie@yahoo.co.uk>
2025-06-06 22:02:55 +0100

    Update API to handle photo upload

--- api/location.js
+++ api/location.js
@@ -1,8 +1,13 @@
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    const { timestamp, coords } = req.body;
+    const { timestamp, coords, photoBase64 } = req.body;
     console.log('Received location:', timestamp, coords);
-    res.status(200).json({ message: 'Location received' });
+
+    if (photoBase64) {
+      console.log('Received photo (Base64):', photoBase64.substring(0, 100) + '...');
+    }
+
+    res.status(200).json({ message: 'Location (and photo if present) received' });
   } else {
     res.status(405).json({ message: 'Method not allowed' });
   }
